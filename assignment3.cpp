b1cf4474cd0186114ada671df91467e4b6f174f1
BarakShenfeld <163340311+BarakShenfeld@users.noreply.github.com>
2024-07-04 15:23:02 +0300

    Add files via upload

---
+++ assignment3.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>    // for printf
+#include <conio.h>    // for getch
+#include <iostream>   // for cin, cout
+#include <fstream>    // for file IO
+#define _USE_MATH_DEFINES
+#include <math.h> 
+#include "ImProcInPlainC.h"
+using namespace std;
